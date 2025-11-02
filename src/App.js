4e2fa64ea7a48fd0d7119779fb4b9107e9c17042
tyang019 <tyang019@fiu.edu>
2025-11-01 23:40:52 -0400

    Initialized new changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Home from "./Home"
 import AboutMe from"./AboutMe"
 import {Routes, Route, Link} from 'react-router-dom';
+import React from "react";
+import ReactPlayer from "react-player";
 
 
 function App() {
@@ -11,6 +13,16 @@ function App() {
         <Link to="/" className='nav-item'>Home</Link>
         <Link to="/about-me" className='nav-item'>About Me</Link>
       </nav>
+      <h1>React Player Video</h1>
+      <div className='App'>
+        <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
+        controls={true} 
+        playing={true}
+        muted={true}
+        width="640px"
+        height="360px"
+      />
+      </div>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/about-me" element={<AboutMe />} />
